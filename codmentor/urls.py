d01dcbb5d7079f0c22b46a796bf11144d1bfe103
Iván Ruiz <info@quintero.cz>
2024-09-02 13:44:01 +0200

    Updating Readme.md

--- codmentor/urls.py
+++ codmentor/urls.py
@@ -20,15 +20,12 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from .views import HomeView, RedirectExerciseView
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('codmentor/', HomeView.as_view(), name='home'),
     path('codmentor/redirect/', RedirectExerciseView.as_view(), name='redirect_exercise'),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
